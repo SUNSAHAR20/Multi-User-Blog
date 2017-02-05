7ea4e35c3fa6ac248b4e64d91e771bedd7eded34
SUNSAHAR20 <sunsahar14@gmail.com>
2017-02-05 19:06:16 +0530

    New commit

--- blogger/comment.py
+++ blogger/comment.py
@@ -2,9 +2,9 @@ from google.appengine.ext import db
 
 from user import User
 
+		#Class that creates a comment object
 class Comment(db.Model):
-    """class that creates the basic database specifics for a comment"""
-    comment = db.TextProperty(required=True)
-    commentauthor = db.StringProperty(required=True)
-    commentid = db.IntegerProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
+	comment = db.TextProperty(required=True)
+	commentauthor = db.StringProperty(required=True)
+	commentid = db.IntegerProperty(required=True)
+	created = db.DateTimeProperty(auto_now_add=True)
